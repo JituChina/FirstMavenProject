962e89702d9cdcc5a8d8345ca64b6497b011afaa
ORACLE <ORACLE@DESKTOP-5QFQ05M>
2022-05-30 10:34:59 +0800

    Class2Demo

---
+++ src/test/java/Class2Test.java
@@ -0,0 +1,8 @@
+// Class 2 git demo
+public class Class2Test {
+
+	public Class2Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/test/java/TestMe.java
+++ src/test/java/TestMe.java
@@ -1,4 +1,5 @@
-// This is to test GitHub
+// This is to test 
+// GitHub class 
 public class TestMe {
 
 	public TestMe() {
